5147aa710eaf8894239ea73b3181852dc033a5c2
Sebastian02L <sebaslf02@gmail.com>
2024-10-12 14:28:31 +0200

    Sistema de almacenamiento de datos Locales con PlayerPrefs
    Primera version de esta funcionalidad, cuando se pueda transicionar entre escenas se debe adaptar para que el usuario pueda escribir sus datos.

---
+++ KOCube/Assets/Scripts/PlayerDataManager.cs
@@ -0,0 +1,142 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PlayerDataManager : Singleton<PlayerDataManager>
+{
+    //Variable que indica el numero de personajes del juego
+    const int numberOfCharacters = 6;
+
+    //Atributos que se almacenaran
+    string username;
+    int coins;
+    List<List<string>> characterSkins = new List<List<string>>(numberOfCharacters);
+    //Por cada personaje se almacena una lista con el codigo de las skisn que posee, siendo 0 la skin por defecto.
+    //Los codigos de los personajes (su orden en la lista) son:
+    //Ma "Xing" Gan 0, Judy 1, Cleo 2, Bale 3, Sadguy 4, Alra 5
+
+
+    void Start()
+    {
+        //Inicizaliacion de la estructura
+        for (int i = 0; i < numberOfCharacters; i++)
+        {
+            characterSkins.Add(new List<string>());
+        }
+
+        //Si existen datos guardados del jugador, los recuperamos y los almacenamos
+        if (PlayerPrefs.HasKey("username"))
+        {
+            username = PlayerPrefs.GetString("username");
+            coins = PlayerPrefs.GetInt("coins");
+
+            for (int i = 0; i < numberOfCharacters; i++)
+            {   //Recuperamos la cadena de skins de cada personaje
+                string stringSkins = PlayerPrefs.GetString("Character" + i);
+                //Convertimos en lista la cadena anterior
+                List<string> skinList = stringSkins.Split('.').ToList();
+                //Asignamos la lista recuperada
+                characterSkins[i] = skinList;
+            }
+
+            Debug.Log("Datos recuperados:" + username + " " + coins + " " + characterSkins.ToString());
+        }
+        else
+        {
+            //Si no hay datos guardados, se crean con los valores iniciales
+            Debug.Log("No hay datos guardados para este jugador. Creando estructura por defecto...");
+
+            SetNameData("jugador"); //Aqui habria que poner el nombre que escriba el jugador
+            SetCoinsData(10000);
+            SetSkinsData("0");
+            SaveData();
+        }
+
+        Debug.Log("Ruta de PlayerPrefs en este sistema operativo: " + Application.persistentDataPath);
+        //Decomentar esta linea para borrar los datos (esto solo esta aqui para pruebas)
+        //DeleteData();
+    }
+
+    //////////////////////// Metodos publicos para modificar datos desde otros scripts //////////////////////// 
+    public void SetName(string name)
+    {
+        username = name;
+        SetNameData(name);
+        SaveData();
+    }
+
+    public void SetCoins(int amount)
+    {
+        coins = amount;
+        SetCoinsData(amount);
+        SaveData();
+    }
+
+    public void SetSkins(string skinCode, int charCode)
+    {
+        characterSkins[charCode].Add(skinCode);
+        SetSkinsData(skinCode, charCode);
+        SaveData();
+    }
+
+    public string GetName()
+    {
+        return username;
+    }
+
+    public int GetCoins()
+    {
+        return coins;
+    }
+
+    public List<string> GetSkins(int charCode)
+    {
+        return characterSkins[charCode];
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.Save();
+    }
+
+    public void DeleteData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+    //////////////////////// Metodos privados que guardan los datos localmente con PlayerPrefs //////////////////////// 
+    void SetNameData(string name)
+    {
+        PlayerPrefs.SetString("username", name);
+    }
+
+    void SetCoinsData(int amount)
+    {
+        PlayerPrefs.SetInt("coins", amount);
+    }
+
+    void SetSkinsData(string skinCode, int charCode = -1) 
+    {
+        //Quiere decir que hay que crear la estructura por defecto de las skins que tiene el jugador
+        if (charCode == -1)
+        {
+            //Por cada personaje, se crea una lista con el codigo de las skins que tenga 
+            for(int i = 0; i < numberOfCharacters; i++)
+            {
+                characterSkins[i].Add("0");
+                PlayerPrefs.SetString("Character" + i, CreateStringSkins(characterSkins[i]));
+            }
+        }
+        else
+        {
+            PlayerPrefs.SetString("Character" + charCode, CreateStringSkins(characterSkins[charCode]));
+        }
+    }
+
+    string CreateStringSkins(List<string> skinList)
+    {
+        return string.Join(".", skinList.ToString());
+    }
+}
