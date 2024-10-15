8c7821b5f30a4a4eaaa3dc91cb447556130ba6f2
Sebastián Loges <sebaslf02@gmail.com>
2024-10-15 09:13:18 +0200

    Ya se puede escribir el codigo en pantallas tactiles

--- KOCube/Assets/Scripts/TouchInputHandler.cs
+++ KOCube/Assets/Scripts/TouchInputHandler.cs
@@ -7,15 +7,24 @@ using UnityEngine.EventSystems;
 public class TouchInputHandler : MonoBehaviour
 {
     public TMP_InputField inputField; // Arrastra tu campo de texto en el inspector
+    private TouchScreenKeyboard keyboard;
 
     // Método que se llamará cuando el objeto sea clickeado
     public void OnSelect(string s)
     {
         Debug.Log("me has pulsado");
         // Activar el teclado en pantalla
+        keyboard = TouchScreenKeyboard.Open("",TouchScreenKeyboardType.Default); // Abre el teclado virtual
         inputField.ActivateInputField();
-        TouchScreenKeyboard.Open(inputField.textComponent.text, TouchScreenKeyboardType.Default); // Abre el teclado virtual
         Debug.Log("supuestamente abri el teclado");
         
+    }
+
+    private void Update()
+    {
+        if (keyboard != null && keyboard.active)
+        {
+            inputField.text = keyboard.text;
+        }
     }
 }
