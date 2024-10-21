74c6453e321d3701eda148629d3648b82b8d9e87
Marcos De Ozaeta Cabadas <marcos.deozaeta@gmail.com>
2024-10-21 21:59:36 +0200

    Ataque doble comentado

--- KOCube/Assets/Scripts/Components/Projectiles/CleoProjectile.cs
+++ KOCube/Assets/Scripts/Components/Projectiles/CleoProjectile.cs
@@ -14,35 +14,38 @@ public class CleoProjectile : AProjectile
 
     public override void CheckDestroy(Collider other) //Cada proyectil tiene sus condiciones de destrucción
     {
-        string otherTag = other.tag;
-        cleoThreeHits = this.attacker.GetComponentInChildren<CleoThreeHitsBehaviour>();
-        if (cleoThreeHits.GetHits() >= 2)
-        {
-            GameObject.Instantiate(this, transform.parent);
-            cleoThreeHits.ResetHits();
-        }
-        else
-        {
-            //En este caso, el proyectil se destruye al chocar con un jugador del otro equipo o con un objeto del mapa
-            if (otherTag.Equals("Team1"))
-            {
-                if (this.tag.Equals("Team2"))
-                {
-                    cleoThreeHits.AddHit();
-                }
-            }
-            else if (otherTag.Equals("Team2"))
-            {
-                if (this.tag.Equals("Team1"))
-                {
-                    cleoThreeHits.AddHit();
-                }
-            }
-            else
-            {
-                cleoThreeHits.ResetHits();
-            }
-        }
+        //string otherTag = other.tag;
+        //if (this.attacker != null)
+        //{
+        //    if (other.GetComponent<PlayerBehaviour>() == this.attacker) return;
+        //    cleoThreeHits = this.attacker.GetComponentInChildren<CleoThreeHitsBehaviour>();
+        //    if (cleoThreeHits.GetHits() >= 2)
+        //    {
+        //        CleoProjectile newProjectile = GameObject.Instantiate(this, transform.parent);
+        //        cleoThreeHits.ResetHits();
+        //    }
+        //    else
+        //    {
+        //        //En este caso, el proyectil se destruye al chocar con un jugador del otro equipo o con un objeto del mapa
+        //        if (otherTag.Equals("Team1"))
+        //        {
+        //            if (this.tag.Equals("Team2"))
+        //            {
+        //                cleoThreeHits.AddHit();
+        //                return;
+        //            }
+        //        }
+        //        else if (otherTag.Equals("Team2"))
+        //        {
+        //            if (this.tag.Equals("Team1"))
+        //            {
+        //                cleoThreeHits.AddHit();
+        //                return;
+        //            }
+        //        }
+        //            cleoThreeHits.ResetHits();
+        //    }
+        //}
         
     }
 }
