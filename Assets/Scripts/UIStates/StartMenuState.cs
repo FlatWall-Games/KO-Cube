b278b0df467e8371e40afed048be618c28b56ade
Sebastián Loges <sebaslf02@gmail.com>
2024-11-19 10:50:18 +0100

    0.17.1 Hotfix
    * Se muestra la versión de la app en la pantalla de inicio (debe aumentarse a mano cada vez que se buildee)
    * Se cambió el nombre de la compañía para intentar ver si sigue encontrando las PlayerPrefs entre builds.

--- Assets/Scripts/UIStates/StartMenuState.cs
+++ Assets/Scripts/UIStates/StartMenuState.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -16,6 +17,7 @@ public class StartMenuState : AUIState
         startMenu = contextUI.Canvas.transform.Find("StartMenu").gameObject;
         startMenu.SetActive(true);
         contextUI.Canvas.transform.Find("Shop").gameObject.SetActive(false);
+        startMenu.transform.Find("Version").GetComponent<TextMeshProUGUI>().text = "Version " + Application.version;
     }
 
     public override void Exit()
