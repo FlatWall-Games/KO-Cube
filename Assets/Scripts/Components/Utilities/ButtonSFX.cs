cd7427a7d4c1d02b121ce80b248c3182094db1a6
Kares <carlosegmt23@gmail.com>
2024-11-22 16:58:50 +0100

    0.23.0 animaciones implementadas
    Las animaciones de los personajes están implementadas e interpoladas.

--- Assets/Scripts/Components/Utilities/ButtonSFX.cs
+++ Assets/Scripts/Components/Utilities/ButtonSFX.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class ButtonSFX : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
+public class ButtonSFX : MonoBehaviour, IPointerEnterHandler, ISelectHandler
 {
+    private void Awake()
+    {
+        GetComponent<Button>()?.onClick.AddListener(OnButtonClick);
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         GameObject.FindObjectOfType<SFXManager>().PlaySFX(0, 0.2f);
     }
 
-    public void OnPointerClick(PointerEventData eventData)
+    public void OnButtonClick()
     {
         GameObject.FindObjectOfType<SFXManager>().PlaySFX(1, 0.4f);
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        GameObject.FindObjectOfType<SFXManager>().PlaySFX(0, 0.2f);
     }
 }
